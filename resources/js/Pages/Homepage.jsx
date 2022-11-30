e2f47e8609b5e3598feda3de44734c9978301f15
davidwah <davidwahyuyu@gmail.com>
2022-11-30 21:14:26 +0700

    update pagination validation

--- resources/js/Pages/Homepage.jsx
+++ resources/js/Pages/Homepage.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 import { Link, Head } from '@inertiajs/inertia-react';
 import Navbar from "@/Components/Navbar";
 import NewsList from "@/Components/Homepage/NewsList";
+import Pagination from "@/Components/Homepage/Pagination";
 
 export default function Homepage(props) {
-    console.log('props: ',props)
     return (
         <div className='min-h-screen bg-slate-50'>
             <Head title={props.title} />
@@ -12,6 +12,9 @@ export default function Homepage(props) {
             {/*<p>{props.description}</p>*/}
             <div className='flex justify-center flex-col lg:flex-row lg:flex-wrap lg:items-stretch items-center gap-4 p-4'>
                 <NewsList news={props.news.data}/>
+            </div>
+            <div className='flex justify-center items-center'>
+                <Pagination meta={props.news.meta}/>
             </div>
 
         </div>
